9faa9947a039e786f15330d5519e76ef254a0eaa
7honarias <7honarias@gmail.com>
2021-06-03 13:44:08 -0500

    [ADD] - configuration of spring

---
+++ src/main/java/com/arioval/roulette/ApiRouletteBetApplication.java
@@ -0,0 +1,14 @@
+package com.arioval.roulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRouletteBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRouletteBetApplication.class, args);
+		System.out.println("welcome");
+	}
+
+}
